76f0d80bb17e55ac762f2d4bdb5a0d6edbba858a
Rahul Dey <rahuldey832000@gmail.com>
2024-03-03 11:56:51 +0530

    As Parallel Method

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Diagnostics;
+
+var stopwatch = Stopwatch.StartNew();
+
+var collection = Enumerable.Range(0, 10)
+    .AsParallel()
+    .Select(HeavyComputation);
+
+foreach (var _ in collection);
+stopwatch.Stop();
+Console.WriteLine($"Execution Time: {stopwatch.ElapsedMilliseconds}");
+
+int HeavyComputation(int n)
+{
+    for(int j = 0;j < 100000000; j++)
+    {
+        n += j;
+    }
+    //Console.WriteLine(n);
+
+    return n;
+}
